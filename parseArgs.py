dc0f4904b589b304be65e7284c91c5b5477c2904
Caitlin Ann Patton <kittyluva2@gmail.com>
2014-10-12 12:03:15 -0700

    Parse Args stuff

---
+++ parseArgs.py
@@ -0,0 +1,19 @@
+#!/Library/Frameworks/Python.framework/Versions/2.7/bin/python
+
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument("square", help="Display a square of a given number", type=int)
+args = parser.parse_args()
+print args.square**2
+
+parser.add_argument("-v", "--verbose", action="count", default=0, help="increase output verbosity")
+args = parser.parse_args()
+answer = args.square**2
+
+if args.verbose >= 2:
+	print "The square of {} equals {}".format(args.square, answer)
+elif args.verbose >= 1:
+	print "{}^2 == {}".format(args.square, answer)
+else:
+	print answer
