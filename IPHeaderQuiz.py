7a52c2c0658cdbc49f47ad7084f5c2352e045f2d
Caitlin Ann Patton <kittyluva2@gmail.com>
2014-11-26 13:03:02 -0800

    Quiz time!

--- IPHeaderQuiz.py
+++ IPHeaderQuiz.py
@@ -35,3 +35,44 @@ if total_lenth == 16:
 else:
 	print "Wrong!"
 
+identification = int(raw_input("What is the bit size of the identification?: "))
+if identification == 16:
+	print "Correct!"
+else:
+	print "Wrong!"
+
+bit_flags = int(raw_input("What is the length of the bit flags?: "))
+if bit_flags == 3:
+	print "Correct!"
+else:
+	print "Wrong!"
+
+frag_offset = int(raw_input("What is the length of the fragment offset in bits?: "))
+if frag_offset == 13:
+	print "Correct!"
+else:
+	print "Wrong!"
+
+time_to_live = int(raw_input("What is the bit size of the time to live?: "))
+if time_to_live == 8:
+	print "Correct!"
+else:
+	print "Wrong!"
+
+checksum = int(raw_input("What is the bit size of the header checksum?: "))
+if checksum == 16:
+	print "Correct!"
+else:
+	print "Wrong!"
+
+source_ip = int(raw_input("What is the bit length of the source ip address?: "))
+if source_ip == 32:
+	print "Correct!"
+else:
+	print "Wrong!"
+
+destination_ip = int(raw_input("What is the bit length of the destination ip address?: "))
+if destination_ip == 32:
+	print "Correct!"
+else:
+	print "Wrong!"
