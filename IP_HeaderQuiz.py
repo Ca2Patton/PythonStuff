4bbb4fa8800a5ec4a86ce63dd134970493f54f30
Caitlin Ann Patton <kittyluva2@gmail.com>
2014-11-26 14:35:26 -0800

    Another quiz in the works

--- IP_HeaderQuiz.py
+++ IP_HeaderQuiz.py
@@ -3,7 +3,7 @@
 QandA = {"What is the total byte size of an IP header? (excluding any options and data): " : 20, "What's the total bit size of each block in an IP header?: " : 32, "How many bits is the version?: " : 4, "What is the bit size of the header length?: " : 4, "What is the bit length of the type of service?: " : 8, "What bit size is the total length?: " : 16, "What is the bit size of the identification?: " : 16, "What is the length of the bit flags?: " : 3, "What is the length of the fragment offset in bits?: " : 13, "What is the bit size of the time to live?: " : 8, "What is the bit size of the protocol?: " : 8, "What is the bit size of the header checksum?: " : 16, "What is the bit length of the source ip address?: " : 32, "What is the bit length of the destination ip address?: " : 32}
 
 for q in QandA:
-	question = QandA.iterkeys()
+	question = QandA.itervalues()
 	answer = raw_input(question)
 	if answer == question:
 		print "Correct!"
