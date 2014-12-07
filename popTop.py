6fce919fd951316f1f3d16b8bb56b438bfeb7ef6
Caitlin Ann Patton <kittyluva2@gmail.com>
2014-12-06 18:42:17 -0800

    Half of top emulated using a dictionary

--- popTop.py
+++ popTop.py
@@ -3,6 +3,11 @@
 import psutil
 import sys
 import os
-import subprocess
 
-tops = subprocess.call("top")
+for pids in psutil.process_iter():
+	try:
+		pinfo = pids.as_dict(attrs=['pid','name', 'username','uids','terminal','cpu_percent','memory_percent', 'status'])
+	except psutil.NoSuchProcess:
+		pass
+	else:
+		print(pinfo)
