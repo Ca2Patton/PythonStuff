73aefc387a90eb4ad2219316db0db5898c7234ea
Caitlin Ann Patton <kittyluva2@gmail.com>
2014-12-09 17:04:38 -0800

    Made output look pre pretty\n, sort of.

--- popTop.py
+++ popTop.py
@@ -4,10 +4,11 @@ import psutil
 import sys
 import os
 
+print "{0:<8}{1:<40}{2:<10}{3:<20}{4:<10}{5:<10}{6:<10}{7:<10}".format("PID", "Name", "User", "Uid", "TTY", "CPU %", "Memory %", "Status")
 for pids in psutil.process_iter():
 	try:
 		pinfo = pids.as_dict(attrs=['pid','name', 'username','uids','terminal','cpu_percent','memory_percent', 'status'])
 	except psutil.NoSuchProcess:
 		pass
 	else:
-		print(pinfo)
+		print "{0:<8}{1:<40}{2:<10}{3:<50}{4:<10}{5:<10}{6:<10}{7:<10}".format(pinfo['pid'], pinfo['name'], pinfo['username'], pinfo['uids'],pinfo['terminal'], pinfo['cpu_percent'], pinfo['memory_percent'], pinfo['status'])
